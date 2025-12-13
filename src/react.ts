b2481f31a0e819880c5b39eed34e217f9311d54e
anirudhb <anirudhb@users.noreply.github.com>
2025-12-13 13:30:37 -0800

    prep for separating package

--- src/react.ts
+++ src/react.ts
@@ -1,24 +1,104 @@
 /** React hooking tools */
 
 import * as webpack from "./webpack";
-
-export function init() {
-  /* Populate modules */
-  globalThis.React = webpack.tryPopulateModule("react", m => m.createElement || m.Component || m.useState) as typeof import("react");
-  let dom1 = webpack.tryPopulateModule("react-dom", m => m.render || m.createPortal);
-  let dom2 = webpack.tryPopulateModule("react-dom/client", m => m.createRoot || m.hydrateRoot);
-  globalThis.ReactDOM = {...dom1, ...dom2} as typeof import("react-dom") & typeof import("react-dom/client");
-}
-
-/* FIXME: very hacky */
-//let React = utils.forwardingProxy(() => globalThis.React);
-//let ReactDOM = utils.forwardingProxy(() => globalThis.ReactDOM);
+import * as utils from "./utils";
 
 // FIXME: what to do about normal class components?
 let __reactComponentRegistry = new Map<string, React.FC>();
 
 if (globalThis.__reactComponentRegistry)
   __reactComponentRegistry = globalThis.__reactComponentRegistry;
+
+// patches for named components
+let __reactPatchRegistry = new Map<string, (c: React.FC) => React.FC>();
+let __reactPatchCache = new Map<React.FC, Map<any, React.FC>>();
+
+declare global {
+  var __real_createElement: typeof import("react").createElement;
+}
+
+export function init() {
+  /* Populate modules */
+  globalThis.React = webpack.tryPopulateModule("react", m => m.createElement || m.Component || m.useState) as typeof import("react");
+  const dom1 = webpack.tryPopulateModule("react-dom", m => m.render || m.createPortal);
+  const dom2 = webpack.tryPopulateModule("react-dom/client", m => m.createRoot || m.hydrateRoot);
+  globalThis._ReactDOM = dom1;
+  globalThis._ReactDOMClient = dom2;
+  globalThis.ReactDOM = {...dom1, ...dom2} as typeof import("react-dom") & typeof import("react-dom/client");
+
+  /* Save createElement */
+  globalThis.__real_createElement = globalThis.React.createElement;
+  /* Hook */
+  globalThis.React.createElement = createElement;
+}
+
+function patchOrCache(x: React.FC, patcher: (c: React.FC) => React.FC): React.FC {
+  if (__reactPatchCache.has(x)) {
+    const c1 = __reactPatchCache.get(x);
+    if (c1.has(patcher)) {
+      return c1.get(patcher);
+    } else {
+      const r = patcher(x);
+      c1.set(patcher, r);
+      return r;
+    }
+  } else {
+    const r = patcher(x);
+    const c1 = new Map();
+    c1.set(patcher, r);
+    __reactPatchCache.set(x, c1);
+    return r;
+  }
+}
+
+/**
+ * Hooked createElement
+ */
+const createElement = function(type: any, props: any, ...children: any[]) {
+  // devirtualize
+  if (type.__isForwardedProxy)
+    type = type.__thunkValue;
+  const name = typeof type === "string" ? type : getComponentName(type);
+  if (__reactPatchRegistry.has(name)) {
+    type = patchOrCache(type, __reactPatchRegistry.get(name));
+  }
+  return globalThis.__real_createElement(type, props, ...children);
+} as typeof globalThis.React.createElement;
+
+/**
+ * Patches the given named component and returns the revoke function.
+ */
+export function patchComponent<P = {}>(name: string, patcher: (c: React.FC<P>) => React.FC<P>): () => void {
+  let revoked = false;
+  const prevPatch = __reactPatchRegistry.get(name) ?? null;
+  const patcher2 = (old: React.FC<P>) => {
+    let old2 = (props: P) => globalThis.__real_createElement(old, props);
+    if (prevPatch !== null)
+      old2 = prevPatch(old2) as any;
+    if (revoked) {
+      //console.log(`patcher running for ${name} ${sym.toString()} (revoked)`);
+      return old2;
+    } else {
+      //console.log(`patcher running for ${name} ${sym.toString()} (real)`);
+      return patcher(old2);
+    }
+  };
+  __reactPatchRegistry.set(name, patcher2);
+  // XXX: can't just remove the patcher entry here and restore
+  // prevPatch because there could have been more patches added.
+  return () => {
+    revoked = true;
+    // FIXME: a better way to do this
+    __reactPatchCache.clear();
+  };
+}
+
+/*
+ * patchComponent with friendlier type annotations.
+ */
+export function patchComponent2<C extends React.FC>(c: C, patcher: (c: C) => (p: React.ComponentProps<C>) => C): () => void {
+  return patchComponent<React.ComponentProps<C>>(getComponentName(c), patcher as any);
+}
 
 /*
  * Tries to find the name of the given React component/whatever.
@@ -46,75 +126,33 @@ export function tryFindReactComponent<P = {}>(name: string): React.FC<P> | null 
 
 /**
  * Tries to populate a React component by the given name. Returns the matched component if any.
+ * If the given component already exists, returns that instead.
  */
 export function tryPopulateReactComponent<P = {}>(name: string): React.FC<P> | null {
-  const c = tryFindReactComponent<P>(name);
+  let c = __reactComponentRegistry.get(name) ?? null;
+  if (c === null)
+    c = tryFindReactComponent<P>(name);
   if (c != null)
     __reactComponentRegistry.set(name, c);
   return c;
 }
 
 /**
- * Rewrites a createElement tree using __reactComponentRegistry, component names prefixed with "Component$".
- * Drop-in replacement for React.createElement
- */
-export function rewriteTree(tree: React.ReactElement): React.ReactElement {
-  /* primitives */
-  if (typeof tree !== "object")
-    return tree;
-
-  let ty = tree.type;
-  if (typeof tree.type === "string" && tree.type.startsWith("Component$")) {
-    const name = tree.type.substring("Component$".length);
-    let c = __reactComponentRegistry.get(name);
-    if (c === undefined) {
-      /* try to populate */
-      c = tryPopulateReactComponent(name);
-    }
-    if (c) {
-      ty = c;
-    } else {
-      console.warn(`Failed to rewrite tree: couldn't find component ${tree.type}`);
-    }
-  }
-
-  return {
-    ...tree,
-    type: ty,
-    props: typeof tree.props === "object" ? {
-      ...tree.props,
-      ...("children" in tree.props ? { children: rewriteTree((tree.props as any).children) } : {}),
-    } : tree.props,
-  };
-}
-
-/**
  * Thunk dispenser that creates virtual components by name.
  */
 export function virtualComponent<P = {}>(name: string): React.FC<P> {
-  function f(props: P): React.ReactNode {
-    return globalThis.React.createElement(`Component\$${name}`, props);
-  }
-  return f;
-}
-
-/**
- * Populates a component and returns a virtual thunk to it.
- */
-export function tryVirtualizeReactComponent<P = {}>(name: string): React.FC<P> {
-  tryPopulateReactComponent(name);
-  return virtualComponent(name);
+  //return (props) => globalThis.React.createElement(`Component\$${name}`, props);
+  return utils.forwardingProxy(() => tryPopulateReactComponent(name));
 }
 
 /** Expose on window */
 let o = {
   __reactComponentRegistry,
   getComponentName,
+  patchComponent,
   tryFindReactComponent,
   tryPopulateReactComponent,
-  rewriteTree,
   virtualComponent,
-  tryVirtualizeReactComponent,
 };
 
 for (const [k, v] of Object.entries(o)) {
--- src/utils.ts
+++ src/utils.ts
@@ -3,15 +3,24 @@
 // Creates a forwarding thunk *readonly* Proxy.
 // Useful when early-binding to a late-bound object.
 // Attempting to modify the given Proxy is a no-op.
-// *This is slow!*
-// Every operation on the Proxy calls the thunk, so try to avoid using this.
 export function forwardingProxy<T extends object = any>(thunk: () => T): T {
-  return new Proxy<T>(Object.create(null), {
-    apply: (target, thiz, args) => Reflect.apply(thunk() as any, thiz, args),
+  return new Proxy<T>((function() {}) as any, {
+    apply: (target, thiz, args) => {
+      const t = thunk() as any;
+      return Reflect.apply(t, t, args);
+    },
     construct: (target, args, newTarget) => Reflect.construct(thunk() as any, args, newTarget),
     defineProperty: (target, prop, attrs) => false,
     deleteProperty: (target, p) => false,
-    get: (target, p, receiver) => Reflect.get(thunk(), p, thunk()),
+    get: (target, p, receiver) => {
+      if (p === "__isForwardedProxy") {
+        return true;
+      } else if (p === "__thunkValue") {
+        return thunk();
+      }
+      const t = thunk() as any;
+      return Reflect.get(t, p, t);
+    },
     getOwnPropertyDescriptor: (target, p) => Reflect.getOwnPropertyDescriptor(thunk(), p),
     getPrototypeOf: (target) => Reflect.getPrototypeOf(thunk()),
     has: (target, p) => Reflect.has(thunk(), p),
